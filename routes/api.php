e2ab027fe048d0d86e5b4f7823f3dd96e310174f
samiha0106 <94045844+samiha0106@users.noreply.github.com>
2021-11-25 12:24:30 +0600

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+ Route::post('/student','ApiController@create');
+ Route::get('/students','ApiController@show');
+ Route::get('/student/{id}','ApiController@showbyid');
+ Route::put('/studentupdate/{id}','ApiController@updatebyid');
+ Route::delete('/studentdelete/{id}','ApiController@deletebyid');
